d8cab38910f9d0fbd4c99e809b31ae2016dffa0e
Allen Wild <allenwild93@gmail.com>
2023-09-15 15:39:37 -0400

    Revert "make print_all and other library functions return std::io::Result<()>"
    Reverting to reduce impending merge conflicts.
    
    This reverts commit 488f0a0aa6f163a371f2a31056d6f8e92251971e.

--- src/lib.rs
+++ src/lib.rs
@@ -213,7 +213,7 @@ impl<W: Write> Printer<W> {
         self
     }
 
-    fn write_border(&mut self, border_elements: BorderElements) -> io::Result<()> {
+    fn write_border(&mut self, border_elements: BorderElements) {
         let h = border_elements.horizontal_line;
         let c = border_elements.column_separator;
         let l = border_elements.left_corner;
@@ -222,42 +222,40 @@ impl<W: Write> Printer<W> {
         let h25 = h.to_string().repeat(25);
 
         if self.show_position_panel {
-            write!(self.writer, "{l}{h8}{c}", l = l, c = c, h8 = h8)?;
+            write!(self.writer, "{l}{h8}{c}", l = l, c = c, h8 = h8).ok();
         } else {
-            write!(self.writer, "{}", l)?;
+            write!(self.writer, "{}", l).ok();
         }
 
-        write!(self.writer, "{h25}{c}{h25}", c = c, h25 = h25)?;
+        write!(self.writer, "{h25}{c}{h25}", c = c, h25 = h25).ok();
 
         if self.show_char_panel {
-            writeln!(self.writer, "{c}{h8}{c}{h8}{r}", c = c, h8 = h8, r = r)?;
+            writeln!(self.writer, "{c}{h8}{c}{h8}{r}", c = c, h8 = h8, r = r).ok();
         } else {
-            writeln!(self.writer, "{r}", r = r)?;
+            writeln!(self.writer, "{r}", r = r).ok();
         }
-        Ok(())
     }
 
-    pub fn print_header(&mut self) -> io::Result<()> {
+    pub fn print_header(&mut self) {
         if self.header_was_printed {
-            return Ok(());
+            return;
         }
         if let Some(e) = self.border_style.header_elems() {
-            self.write_border(e)?;
+            self.write_border(e)
         }
         self.header_was_printed = true;
-        Ok(())
     }
 
-    pub fn print_footer(&mut self) -> io::Result<()> {
+    pub fn print_footer(&mut self) {
         if let Some(e) = self.border_style.footer_elems() {
-            self.write_border(e)?;
+            self.write_border(e)
         }
-        Ok(())
     }
 
-    fn print_position_panel(&mut self) -> io::Result<()> {
+    fn print_position_panel(&mut self) {
         if !self.show_position_panel {
-            return write!(&mut self.buffer_line, "{} ", self.border_style.outer_sep());
+            write!(&mut self.buffer_line, "{} ", self.border_style.outer_sep()).ok();
+            return;
         }
 
         let style = COLOR_OFFSET.normal();
@@ -267,40 +265,41 @@ impl<W: Write> Printer<W> {
         } else {
             byte_index
         };
-        write!(
+        let _ = write!(
             &mut self.buffer_line,
             "{}{}{} ",
             self.border_style.outer_sep(),
             formatted_string,
             self.border_style.outer_sep()
-        )
+        );
     }
 
-    pub fn print_char_panel(&mut self) -> io::Result<()> {
+    pub fn print_char_panel(&mut self) {
         if !self.show_char_panel {
             // just write newline if character panel is hidden
-            return writeln!(&mut self.buffer_line);
+            writeln!(&mut self.buffer_line).ok();
+            return;
         }
 
         let len = self.raw_line.len();
 
         let mut idx = 1;
         for &b in self.raw_line.iter() {
-            write!(
+            let _ = write!(
                 &mut self.buffer_line,
                 "{}",
                 self.byte_char_panel[b as usize]
-            )?;
+            );
 
             if idx == 8 {
-                write!(&mut self.buffer_line, "{}", self.border_style.inner_sep())?;
+                let _ = write!(&mut self.buffer_line, "{}", self.border_style.inner_sep());
             }
 
             idx += 1;
         }
 
         if len < 8 {
-            writeln!(
+            let _ = writeln!(
                 &mut self.buffer_line,
                 "{0:1$}{3}{0:2$}{4}",
                 "",
@@ -308,22 +307,22 @@ impl<W: Write> Printer<W> {
                 8,
                 self.border_style.inner_sep(),
                 self.border_style.outer_sep(),
-            )
+            );
         } else {
-            writeln!(
+            let _ = writeln!(
                 &mut self.buffer_line,
                 "{0:1$}{2}",
                 "",
                 16 - len,
                 self.border_style.outer_sep()
-            )
+            );
         }
     }
 
     pub fn print_byte(&mut self, b: u8) -> io::Result<()> {
         if self.idx % 16 == 1 {
-            self.print_header()?;
-            self.print_position_panel()?;
+            self.print_header();
+            self.print_position_panel();
         }
 
         write!(&mut self.buffer_line, "{}", self.byte_hex_panel[b as usize])?;
@@ -333,7 +332,7 @@ impl<W: Write> Printer<W> {
 
         match self.idx % 16 {
             8 => {
-                write!(&mut self.buffer_line, "{} ", self.border_style.inner_sep())?;
+                let _ = write!(&mut self.buffer_line, "{} ", self.border_style.inner_sep());
             }
             0 => {
                 self.print_textline()?;
@@ -351,8 +350,8 @@ impl<W: Write> Printer<W> {
 
         if len == 0 {
             if self.squeezer.active() {
-                self.print_position_panel()?;
-                writeln!(
+                self.print_position_panel();
+                let _ = writeln!(
                     &mut self.buffer_line,
                     "{0:1$}{4}{0:2$}{5}{0:3$}{4}{0:3$}{5}",
                     "",
@@ -361,7 +360,7 @@ impl<W: Write> Printer<W> {
                     8,
                     self.border_style.inner_sep(),
                     self.border_style.outer_sep(),
-                )?;
+                );
                 self.writer.write_all(&self.buffer_line)?;
             }
             return Ok(());
@@ -371,7 +370,7 @@ impl<W: Write> Printer<W> {
 
         if squeeze_action != SqueezeAction::Delete {
             if len < 8 {
-                write!(
+                let _ = write!(
                     &mut self.buffer_line,
                     "{0:1$}{3}{0:2$}{4}",
                     "",
@@ -379,19 +378,19 @@ impl<W: Write> Printer<W> {
                     1 + 3 * 8,
                     self.border_style.inner_sep(),
                     self.border_style.outer_sep(),
-                )?;
+                );
             } else {
-                write!(
+                let _ = write!(
                     &mut self.buffer_line,
                     "{0:1$}{2}",
                     "",
                     3 * (16 - len),
                     self.border_style.outer_sep()
-                )?;
+                );
             }
         }
 
-        self.print_char_panel()?;
+        self.print_char_panel();
 
         match squeeze_action {
             SqueezeAction::Print => {
@@ -402,7 +401,7 @@ impl<W: Write> Printer<W> {
                 } else {
                     String::from("*")
                 };
-                writeln!(
+                let _ = writeln!(
                     &mut self.buffer_line,
                     "{5}{0}{1:2$}{5}{1:3$}{6}{1:3$}{5}{1:4$}{6}{1:4$}{5}",
                     asterisk,
@@ -412,7 +411,7 @@ impl<W: Write> Printer<W> {
                     8,
                     self.border_style.outer_sep(),
                     self.border_style.inner_sep(),
-                )?;
+                );
             }
             SqueezeAction::Delete => self.buffer_line.clear(),
             SqueezeAction::Ignore => (),
@@ -434,7 +433,10 @@ impl<W: Write> Printer<W> {
 
     /// Loop through the given `Reader`, printing until the `Reader` buffer
     /// is exhausted.
-    pub fn print_all<Reader: Read>(&mut self, mut reader: Reader) -> io::Result<()> {
+    pub fn print_all<Reader: Read>(
+        &mut self,
+        mut reader: Reader,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let mut buffer = [0; BUFFER_SIZE];
         'mainloop: loop {
             let size = reader.read(&mut buffer)?;
@@ -453,24 +455,25 @@ impl<W: Write> Printer<W> {
         }
 
         // Finish last line
-        self.print_textline()?;
+        self.print_textline().ok();
 
         if !self.header_was_printed() {
-            self.print_header()?;
+            self.print_header();
             if self.show_position_panel {
-                write!(self.writer, "{0:9}", "│")?;
+                write!(self.writer, "{0:9}", "│").ok();
             }
             write!(
                 self.writer,
                 "{0:2}{1:24}{0}{0:>26}",
                 "│", "No content to print"
-            )?;
+            )
+            .ok();
             if self.show_char_panel {
-                write!(self.writer, "{0:>9}{0:>9}", "│")?;
+                write!(self.writer, "{0:>9}{0:>9}", "│").ok();
             }
-            writeln!(self.writer)?;
+            writeln!(self.writer).ok();
         }
-        self.print_footer()?;
+        self.print_footer();
 
         Ok(())
     }
